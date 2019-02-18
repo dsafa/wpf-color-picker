49984be045977bf7f5c361b151386fc3d7a455b2
Brandon Chong <brndnchong@gmail.com>
2019-02-17 23:05:22 -0800

    Add color to hue adorner

--- src/WpfColorPicker/HuePicker.xaml.cs
+++ src/WpfColorPicker/HuePicker.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,17 +23,28 @@ namespace WpfColorPicker
     {
         private static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(HuePicker));
         private readonly HuePickerAdorner _adorner;
+        private byte[] _pixels;
+        private int _stride;
+        private MemoryStream _ms;
 
         public HuePicker()
         {
             InitializeComponent();
             _adorner = new HuePickerAdorner(hueRectangle);
             Loaded += HuePickerOnLoaded;
+            SizeChanged += HuePickerOnSizeChanged;
+        }
+
+        private void HuePickerOnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(this);
         }
 
         private void HuePickerOnLoaded(object sender, RoutedEventArgs e)
         {
             AdornerLayer.GetAdornerLayer(this).Add(_adorner);
+            Snapshot();
         }
 
         public Color SelectedColor
@@ -59,9 +71,53 @@ namespace WpfColorPicker
             UpdateAdorner(e.GetPosition(this));
         }
 
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            Snapshot();
+        }
+
         private void UpdateAdorner(Point mousePos)
         {
             _adorner.VerticalPercent = mousePos.Y / ActualHeight;
+            _adorner.Color = GetColor((int)mousePos.Y);
+        }
+
+        private void Snapshot()
+        {
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(hueRectangle);
+
+            _ms?.Dispose();
+            _ms = new MemoryStream();
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            encoder.Save(_ms);
+
+            var imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.StreamSource = _ms;
+            imageSource.EndInit();
+
+            _stride = imageSource.PixelWidth * 4;
+            int size = imageSource.PixelHeight * _stride;
+            _pixels = new byte[size];
+            imageSource.CopyPixels(_pixels, _stride, 0);
+        }
+
+        private Color GetColor(int y)
+        {
+            if (_pixels == null)
+            {
+                return Colors.Red;
+            }
+
+            int index = y * _stride;
+            var b = _pixels[index];
+            var g = _pixels[index + 1];
+            var r = _pixels[index + 2];
+            var a = _pixels[index + 3];
+            return Color.FromArgb(a, r, g, b);
         }
     }
 }
--- src/WpfColorPicker/HuePickerAdorner.cs
+++ src/WpfColorPicker/HuePickerAdorner.cs
@@ -13,7 +13,14 @@ namespace WpfColorPicker
     {
         private static readonly DependencyProperty VerticalPercentProperty =
             DependencyProperty.Register(nameof(VerticalPercent), typeof(double), typeof(HuePickerAdorner), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        private static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(Color), typeof(HuePickerAdorner), new FrameworkPropertyMetadata(Colors.Red, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        private static readonly Pen Pen = new Pen(Brushes.Black, 2);
         private const double TriangleLength = 20;
+        private const double X = 5;
+        private Brush _brush = Brushes.Red;
 
         public HuePickerAdorner(UIElement adornedElement)
             : base(adornedElement)
@@ -27,26 +34,39 @@ namespace WpfColorPicker
             set => SetValue(VerticalPercentProperty, value);
         }
 
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set
+            {
+                SetValue(ColorProperty, value);
+                _brush = new SolidColorBrush(value);
+            }
+        }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
 
             var adornedElementRect = new Rect(AdornedElement.DesiredSize);
-            var x = -5;
             var y = adornedElementRect.Height * VerticalPercent;
 
-            var arrowColor = Brushes.White;
-            var pen = new Pen(Brushes.Black, 2);
-
-            var triangleGeometry = new StreamGeometry();
-            using (var geometryContext = triangleGeometry.Open())
+            var leftTriGeometry = new StreamGeometry();
+            using (var geometryContext = leftTriGeometry.Open())
             {
-                geometryContext.BeginFigure(new Point(x, y + TriangleLength / 2), true, true);
-                geometryContext.LineTo(new Point(x + TriangleLength, y), true, false);
-                geometryContext.LineTo(new Point(x, y - TriangleLength / 2), true, false);
+                geometryContext.BeginFigure(new Point(-X, y + TriangleLength / 2), true, true);
+                geometryContext.LineTo(new Point(-X + TriangleLength, y), true, false);
+                geometryContext.LineTo(new Point(-X, y - TriangleLength / 2), true, false);
             }
 
-            drawingContext.DrawGeometry(arrowColor, pen, triangleGeometry);
+            var rightTriGeometry = leftTriGeometry.Clone();
+            var transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new TranslateTransform(-adornedElementRect.Width, 0));
+            transformGroup.Children.Add(new ScaleTransform(-1, 1));
+            rightTriGeometry.Transform = transformGroup;
+
+            drawingContext.DrawGeometry(_brush, Pen, leftTriGeometry);
+            drawingContext.DrawGeometry(_brush, Pen, rightTriGeometry);
         }
     }
 }
