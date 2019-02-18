e5a5f89665a0f47c63ac1d87442d98c434495bbf
Brandon Chong <brndnchong@gmail.com>
2019-02-17 18:45:53 -0800

    Add hue picker

---
+++ src/WpfColorPicker/HuePicker.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfColorPicker
+{
+    /// <summary>
+    /// Interaction logic for HuePicker.xaml
+    /// </summary>
+    public partial class HuePicker : UserControl
+    {
+        private static readonly DependencyProperty SelectedColorProperty
+            = DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(HuePicker));
+
+        public HuePicker()
+        {
+            InitializeComponent();
+        }
+
+        public Color SelectedColor
+        {
+            get => (Color)GetValue(SelectedColorProperty);
+            set => SetValue(SelectedColorProperty, value);
+        }
+    }
+}
