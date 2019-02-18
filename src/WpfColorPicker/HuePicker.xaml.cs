ad614cc1369db3e69dd821d80f9b3ec0709a9a81
Brandon Chong <brndnchong@gmail.com>
2019-02-17 22:15:34 -0800

    Add adorner to hue picker

--- src/WpfColorPicker/HuePicker.xaml.cs
+++ src/WpfColorPicker/HuePicker.xaml.cs
@@ -20,18 +20,48 @@ namespace WpfColorPicker
     /// </summary>
     public partial class HuePicker : UserControl
     {
-        private static readonly DependencyProperty SelectedColorProperty
-            = DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(HuePicker));
+        private static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(HuePicker));
+        private readonly HuePickerAdorner _adorner;
 
         public HuePicker()
         {
             InitializeComponent();
+            _adorner = new HuePickerAdorner(hueRectangle);
+            Loaded += HuePickerOnLoaded;
+        }
+
+        private void HuePickerOnLoaded(object sender, RoutedEventArgs e)
+        {
+            AdornerLayer.GetAdornerLayer(this).Add(_adorner);
         }
 
         public Color SelectedColor
         {
             get => (Color)GetValue(SelectedColorProperty);
             set => SetValue(SelectedColorProperty, value);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+
+            UpdateAdorner(e.GetPosition(this));
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            UpdateAdorner(e.GetPosition(this));
+        }
+
+        private void UpdateAdorner(Point mousePos)
+        {
+            _adorner.VerticalPercent = mousePos.Y / ActualHeight;
         }
     }
 }
---
+++ src/WpfColorPicker/HuePickerAdorner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfColorPicker
+{
+    class HuePickerAdorner : Adorner
+    {
+        private static readonly DependencyProperty VerticalPercentProperty =
+            DependencyProperty.Register(nameof(VerticalPercent), typeof(double), typeof(HuePickerAdorner), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+        private const double TriangleLength = 20;
+
+        public HuePickerAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+        }
+
+        public double VerticalPercent
+        {
+            get => (double)GetValue(VerticalPercentProperty);
+            set => SetValue(VerticalPercentProperty, value);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            var adornedElementRect = new Rect(AdornedElement.DesiredSize);
+            var x = -5;
+            var y = adornedElementRect.Height * VerticalPercent;
+
+            var arrowColor = Brushes.White;
+            var pen = new Pen(Brushes.Black, 2);
+
+            var triangleGeometry = new StreamGeometry();
+            using (var geometryContext = triangleGeometry.Open())
+            {
+                geometryContext.BeginFigure(new Point(x, y + TriangleLength / 2), true, true);
+                geometryContext.LineTo(new Point(x + TriangleLength, y), true, false);
+                geometryContext.LineTo(new Point(x, y - TriangleLength / 2), true, false);
+            }
+
+            drawingContext.DrawGeometry(arrowColor, pen, triangleGeometry);
+        }
+    }
+}
