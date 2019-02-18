4256ae992a08b9b88b65f1576c9b6394f3df7fff
Brandon Chong <brndnchong@gmail.com>
2019-02-17 18:13:09 -0800

    Add initial saturation/brightness picker

---
+++ src/WpfColorPicker/SaturationBrightnessPicker.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfColorPicker
+{
+    public partial class SaturationBrightnessPicker : UserControl
+    {
+        private static readonly DependencyProperty ColorProperty 
+            = DependencyProperty.Register(nameof(ColorProperty), typeof(Color), typeof(SaturationBrightnessPicker), new PropertyMetadata(Colors.White));
+
+        public SaturationBrightnessPicker()
+        {
+            InitializeComponent();
+        }
+
+        public Color SelectedColor
+        {
+            get
+            {
+                return (Color)GetValue(ColorProperty);
+            }
+
+            set
+            {
+                SetValue(ColorProperty, value);
+            }
+        }
+    }
+}
