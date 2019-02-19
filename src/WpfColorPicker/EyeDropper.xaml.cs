561777d80b200707c6b01825368d8c330088fc07
Brandon Chong <brndnchong@gmail.com>
2019-02-19 13:05:32 -0800

    Update layout and add eye dropper control

---
+++ src/WpfColorPicker/EyeDropper.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfColorPicker
+{
+    /// <summary>
+    /// Interaction logic for EyeDropper.xaml
+    /// </summary>
+    public partial class EyeDropper : UserControl
+    {
+        public EyeDropper()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var screenshot = TakeScreenshot();
+        }
+
+        private Bitmap TakeScreenshot()
+        {
+            var screenBitmap = new Bitmap((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using (var g = Graphics.FromImage(screenBitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, screenBitmap.Size);
+            }
+
+            return screenBitmap;
+        }
+    }
+}
