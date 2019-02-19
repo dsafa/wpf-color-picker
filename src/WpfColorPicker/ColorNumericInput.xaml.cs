bb3e1d8e5e3f8055628d3c1660e85019a5451d12
Brandon Chong <brndnchong@gmail.com>
2019-02-18 18:14:14 -0800

    Add color inputs to control

---
+++ src/WpfColorPicker/ColorNumericInput.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfColorPicker
+{
+    /// <summary>
+    /// Interaction logic for NumericInput.xaml
+    /// </summary>
+    internal partial class ColorNumericInput : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(int), typeof(ColorNumericInput), new PropertyMetadata(0, OnValueChanged));
+        public static readonly DependencyProperty MinProperty = DependencyProperty.Register(nameof(Min), typeof(int), typeof(ColorNumericInput), new PropertyMetadata(0));
+        public static readonly DependencyProperty MaxProperty = DependencyProperty.Register(nameof(Max), typeof(int), typeof(ColorNumericInput), new PropertyMetadata(0));
+        private static readonly Regex regex = new Regex(@"[0-9]+", RegexOptions.Compiled);
+
+        public ColorNumericInput()
+        {
+            InitializeComponent();
+        }
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public int Min
+        {
+            get => (int)GetValue(MinProperty);
+            set => SetValue(MinProperty, value);
+        }
+
+        public int Max
+        {
+            get => (int)GetValue(MaxProperty);
+            set => SetValue(MaxProperty, value);
+        }
+
+        private void TextBoxOnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void ClampValue()
+        {
+            if (Value < Min)
+            {
+                Value = Min;
+            }
+            else if (Value > Max)
+            {
+                Value = Max;
+            }
+        }
+
+        private static void OnValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var input = (ColorNumericInput)o;
+            input.ClampValue();
+        }
+    }
+}
