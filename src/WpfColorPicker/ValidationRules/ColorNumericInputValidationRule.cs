7f7e7f072354f1542d058471fc6fc8944f226a76
Brandon Chong <brndnchong@gmail.com>
2019-02-20 20:57:20 -0800

    Add validation message to input

---
+++ src/WpfColorPicker/ValidationRules/ColorNumericInputValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfColorPicker.ValidationRules
+{
+    internal class ColorNumericInputValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null || (string)value == "")
+            {
+                return new ValidationResult(false, "Please enter a number");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
