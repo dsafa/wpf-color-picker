27f09d5f6e35fa81fc16d6593c681352bb57c601
Benjamin Altpeter <benjamin@oldpeter.de>
2019-09-19 14:30:14 +0200

    Make the eye dropper work with more than one screen
    Previously, the eye dropper was hardcoded to only use the primary
    screen.
    
    This commit changes that behaviour to essentially use the screen
    the mouse is located on and re-take the screenshot as well as move
    the eye dropper window when the mouse moves to a different screen.

--- src/WpfColorPicker/EyeDropper.xaml.cs
+++ src/WpfColorPicker/EyeDropper.xaml.cs
@@ -26,7 +26,37 @@ namespace Dsafa.WpfColorPicker
             set => SetValue(SelectedColorProperty, value);
         }
 
+        public static Rectangle CurrentScreenRect()
+        {
+            return System.Windows.Forms.Screen.FromPoint(System.Windows.Forms.Cursor.Position).Bounds;
+        }
+
         private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var window = new EyeDropperWindow(ScreenshotBitmapImage());
+            // The EyeDropperWindow spans the entire current screen, so this event gets called whenever
+            // the mouse changes from one screen to another.
+            window.MouseLeave += (_s, _e) =>
+            {
+                // Changing the screen rect and image unfortunately causes a short flash of the old
+                // image on the new screen. To circumvent this issue, we first set the window to be
+                // invisible (with a size of 0x0.
+                // This is not ideal but makes the issue less noticable. Actually hiding the window
+                // seems to work better but we can't do that because calling `.Hide()` or setting
+                // `.Visibility` sets the `DialogResult` to `false`.
+                window.SetScreenRect(new Rectangle(0, 0, 0, 0));
+                window.SetImage(ScreenshotBitmapImage());
+                window.SetScreenRect(CurrentScreenRect());
+            };
+            window.SetScreenRect(CurrentScreenRect());
+            var res = window.ShowDialog();
+            if (res.HasValue && res.Value)
+            {
+                SelectedColor = window.SelectedColor;
+            }
+        }
+
+        private BitmapImage ScreenshotBitmapImage()
         {
             var screenshot = TakeScreenshot();
 
@@ -44,20 +74,16 @@ namespace Dsafa.WpfColorPicker
 
             screenshot.Dispose();
 
-            var window = new EyeDropperWindow(bitmapImage);
-            var res = window.ShowDialog();
-            if (res.HasValue && res.Value)
-            {
-                SelectedColor = window.SelectedColor;
-            }
+            return bitmapImage;
         }
 
         private Bitmap TakeScreenshot()
         {
-            var screenBitmap = new Bitmap((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            Rectangle screen = CurrentScreenRect();
+            var screenBitmap = new Bitmap(screen.Width, screen.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
             using (var g = Graphics.FromImage(screenBitmap))
             {
-                g.CopyFromScreen(0, 0, 0, 0, screenBitmap.Size);
+                g.CopyFromScreen(screen.Left, screen.Top, 0, 0, screenBitmap.Size);
             }
 
             return screenBitmap;
